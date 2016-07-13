6f7bfd3f9cdaf0599bc4ebb019739b9f77a0c718
Brandon Wand <wandbrandon@gmail.com>
2016-07-13 15:59:14 -0400

    just changed stuff

--- Project/README.md/app/src/main/java/edu/fau/jeff/discoverycentergithub/MainActivity.java
+++ Project/README.md/app/src/main/java/edu/fau/jeff/discoverycentergithub/MainActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    //test123
+    //fuckschool
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
