88dda47bcf802c2fe46308ad7f36896a2bc96055
Jeffbud98 <@gmail.com>
2016-07-15 15:04:45 -0400

    UI Overhaul
    Sign In+Selection Page

---
+++ Project/README.md/app/src/main/java/edu/fau/jeff/discoverycentergithub/SelectionScreen.java
@@ -0,0 +1,13 @@
+package edu.fau.jeff.discoverycentergithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen);
+    }
+}
