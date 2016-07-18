2932368cdb955d5149248c56e9ece837ae2a2f79
Jeffbud98 <jeffbud98@gmail.com>
2016-07-18 00:47:56 -0400

    Finished Main Menu

---
+++ Project/README.md/app/src/main/java/edu/fau/jeff/discoverycentergithub/ScavengerHunt.java
@@ -0,0 +1,7 @@
+package edu.fau.jeff.discoverycentergithub;
+
+/**
+ * Created by Jeffrey S on 7/17/2016.
+ */
+public class ScavengerHunt {
+}
--- Project/README.md/app/src/main/java/edu/fau/jeff/discoverycentergithub/SelectionScreen.java
+++ Project/README.md/app/src/main/java/edu/fau/jeff/discoverycentergithub/SelectionScreen.java
@@ -1,13 +1,53 @@
 package edu.fau.jeff.discoverycentergithub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SelectionScreen extends AppCompatActivity {
+    private ImageView trivia, scavengerHunt, photoAdventure, settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection_screen);
+        trivia=(ImageView) findViewById(R.id.triviagame);
+        trivia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i0=new Intent(SelectionScreen.this, Trivia.class);
+                startActivity(i0);
+            }
+        });
+
+        scavengerHunt=(ImageView) findViewById(R.id.scavenge);
+        scavengerHunt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1=new Intent(SelectionScreen.this, ScavengerHunt.class);
+                startActivity(i1);
+            }
+        });
+
+        photoAdventure=(ImageView) findViewById(R.id.photo);
+        photoAdventure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i2=new Intent(SelectionScreen.this, PhotoAdventure.class);
+                startActivity(i2);
+            }
+        });
+
+        settings=(ImageView) findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i3=new Intent(SelectionScreen.this, Settings.class);
+                startActivity(i3);
+            }
+        });
+
     }
 }
