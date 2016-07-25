bdf2a94786ed20625a3bd3cfe1d35d218e09b660
Jeffbud98 <@gmail.com>
2016-07-25 13:53:18 -0400

    changes

--- Group Assignments/Group Assignment 4/Project/README.md/app/src/main/java/edu/fau/jeff/discoverycentergithub/PhotoAdventure.java
+++ Group Assignments/Group Assignment 4/Project/README.md/app/src/main/java/edu/fau/jeff/discoverycentergithub/PhotoAdventure.java
@@ -1,16 +1,21 @@
 package edu.fau.jeff.discoverycentergithub;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -19,6 +24,7 @@ import android.widget.TextView;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.DateFormat;
 
 import java.io.File;
@@ -29,68 +35,102 @@ import java.util.Date;
  * Created by Jeffrey S on 7/17/2016.
  */
 public class PhotoAdventure extends AppCompatActivity {
-    private static final int REQUEST_IMAGE = 100;
-    TextView tvPath;
-    ImageView picture;
-    File destination;
-    String imagePath;
+    private static final int REQUEST_IMAGE = 100, SELECT_PICTURE=1;
+    private ImageView picture;
+    private Uri file;
     Button camera, gallery;
+    private String selectedImagePath;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_photo_adventure);
-        //vtvPath = (TextView) findViewById(R.id.textView2);
         picture = (ImageView) findViewById(R.id.imageView3);
-        String name =   dateToString(new Date(),"yyyy-MM-dd-hh-mm-ss");
-        destination = new File(Environment.getExternalStorageDirectory(), name + ".jpg");
+        gallery=(Button)findViewById(R.id.openGallery);
         camera=(Button)findViewById(R.id.openCamera);
+
+
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(destination));
-                startActivityForResult(intent, REQUEST_IMAGE);
+                file = Uri.fromFile(getOutputMediaFile());
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+                startActivityForResult(intent, 100);
             }
         });
-        gallery=(Button)findViewById(R.id.openGallery);
+
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
+                intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                intent.setType("file/*");
-                startActivity(intent);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
             }
         });
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if( requestCode == REQUEST_IMAGE && resultCode == Activity.RESULT_OK ){
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                camera.setEnabled(true);
+            }
+        }
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SELECT_PICTURE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
             try {
-                FileInputStream in = new FileInputStream(destination);
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 10;
-                imagePath = destination.getAbsolutePath();
-                tvPath.setText(imagePath);
-                Bitmap bmp = BitmapFactory.decodeStream(in, null, options);
-                picture.setImageBitmap(bmp);
-            } catch (FileNotFoundException e) {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                Log.d("myApp", String.valueOf(bitmap));
+                picture.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (requestCode == 100) {
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                Log.d("myApp", String.valueOf(bitmap));
+                picture.setImageBitmap(bitmap);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
         }
-        else{
-            tvPath.setText("Request cancelled");
-        }
+
     }
 
-    public String dateToString(Date date, String format) {
-        SimpleDateFormat df = new SimpleDateFormat(format);
-        return df.format(date);
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
     }
+
+
 
 
 
